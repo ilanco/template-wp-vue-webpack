622de6335e8b545260fd239976dc17d3736711bd
Ilan Cohen <ilanco@gmail.com>
2017-01-25 17:02:17 -0600

    add: default sao template

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # template-wp-vue-webpack
+
+> A modern starter theme for Wordpress which uses Vue 2, Vuex, Vue-router and Webpack 2
+
+## Usage
+
+Install [SAO](https://github.com/egoist/sao) first.
+
+### From git
+
+```bash
+sao ilanco/template-wp-vue-webpack
+```
+
+### From npm
+
+```bash
+sao wp-vue-webpack
+```
+
+## License
+
+MIT &copy; [ilanco](github.com/ilanco)
---
+++ sao.js
@@ -0,0 +1,37 @@
+const superb = require('superb')
+
+module.exports = {
+  prompts: {
+    name: {
+      message: 'What is the name of the new project?',
+      role: 'folder:name'
+    },
+    description: {
+      message: 'How would you descripe the new project?',
+      default: `my ${superb()} project`
+    },
+    username: {
+      message: 'What is your GitHub username?',
+      role: 'git:name',
+      store: true
+    },
+    email: {
+      message: 'What is your GitHub email?',
+      role: 'git:email',
+      store: true
+    },
+    website: {
+      message: 'The URL of your website?',
+      default({username}) {
+        return `github.com/${username}`
+      },
+      store: true
+    }
+  },
+  post({log, chalk, isNewFolder, folderName}) {
+    log.success('Done!')
+    if (isNewFolder) {
+      log.info(`cd ${chalk.yellow(folderName)} to get started!`)
+    }
+  }
+}
