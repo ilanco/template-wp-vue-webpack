36386ee83824d391a01cb4e0ed29ce7398d9ed95
Ilan Cohen <ilanco@gmail.com>
2017-01-26 16:35:56 -0600

    add: if_or template helper

--- meta.js
+++ meta.js
@@ -1,4 +1,12 @@
 module.exports = {
+  helpers: {
+    if_or: function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+      return options.inverse(this)
+    }
+  },
   prompts: {
     name: {
       type: "string",
--- sao.js
+++ sao.js
@@ -2,6 +2,16 @@ const superb = require('superb')
 
 module.exports = {
   template: 'handlebars',
+  templateOptions: {
+    helpers: {
+      if_or(v1, v2, options) {
+        if (v1 || v2) {
+          return options.fn(this)
+        }
+        return options.inverse(this)
+      }
+    }
+  },
   prompts: {
     name: {
       required: true,
